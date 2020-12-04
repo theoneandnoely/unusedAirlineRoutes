19859ad71eb07cf35500785e6cf1ed02cd0546c3
theoneandnoely <nmarkham96@gmail.com>
2020-12-04 20:38:49 +0000

    Adding getListOfURLs.py and airportDataScrape.py files for the first time

---
+++ airportDataScrape.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 14:22:03 2020
+
+@author: Noel
+"""
+import re
+import requests
+import time
+from bs4 import BeautifulSoup
+from getListOfURLs import getIATAURLs
+import pandas as pd
+
+
+def scrapeAirportData():
+    #Get List of URLs
+    url_list = getIATAURLs()
+    counter = 0
+    data = {}
+    for url in url_list:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "html.parser")
+        #Get IATA Code
+        code_div = soup.find('div', class_='views-field views-field-title-2')
+        code = code_div.find('p').text[11:]
+        #Get Country Name
+        country_div = soup.find('div', class_='views-field views-field-field-strana-eng')
+        country = country_div.find('p').text[9:]
+        #Get City Name
+        city_div = soup.find('div', class_='views-field views-field-field-gorod-eng')
+        city = city_div.find('p').text[6:]
+        #Get Airport Name
+        airport_div = soup.find('div', class_='views-field views-field-field-name-eng-1')
+        airport = airport_div.find('p').text[14:]
+        #Get Latitude and Longitude
+        coor_div = soup.find('div',class_='views-field views-field-field-geo')
+        coor_p = coor_div.find_all('p')
+        for p in coor_p:
+            lat_check = re.search('^Latitude:.*',p.text)
+            lon_check = re.search('^Longitude:.*',p.text)
+            if lat_check:
+                lat_str = p.text[10:]
+                lat_float = float(lat_str)
+            elif lon_check:
+                lon_str = p.text[11:]
+                lon_float = float(lon_str)
+        #Update the data dictionary with the scraped data
+        data[code] = {'Airport Name':airport, 'Country':country, 'City':city, 'Latitude':lat_float, 'Longitude':lon_float}
+        #Add one to counter and sleep for a second every 5 urls
+        counter = counter + 1
+        rest_check = counter%5
+        if rest_check == 0:
+            time.sleep(1)
+    return data
+
+def saveAirportDataAsCSV(data):
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df.to_csv('airport_data.csv')
+    
+if __name__ == '__main__':
+    data = scrapeAirportData()
+    saveAirportDataAsCSV(data)
+    print('Code was executed successfully. Poggers.')
---
+++ getListOfURLs.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 13:46:04 2020
+
+@author: Noel
+"""
+from string import ascii_lowercase as alphabet
+import re
+import requests
+import time
+from bs4 import BeautifulSoup
+
+
+def getLetterURLs():
+    '''
+    Function to create list of URLs ending in every letter of the alphabet from a base url
+    '''
+    url_base = 'https://airports-list.com/iata-code'
+    url_list = []
+    for c in alphabet:
+        url = url_base + '/' + c
+        url_list.append(url)
+    return url_list
+
+def getIATAcodes():
+    '''
+    Function to create a list of IATA code appendices for creating URLs
+    
+    Returns: IATA_list -- a list of strings in the form "/airport/<IATA code>"
+
+    '''
+    #import list of URLs
+    url_list = getLetterURLs()
+    #initiate list of codes
+    IATA_list = []
+    #Parse the html for each of the urls
+    for url in url_list:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "html.parser")
+        #find only the links that use the "/airport/<IATA code>" format
+        tags = soup.find_all('a', {"href" : re.compile('^/airport/.*')})
+        #Check the link isn't already in the list and append it
+        for t in tags:
+            link = t.attrs['href']
+            if link in IATA_list:
+                pass
+            else:
+                IATA_list.append(link)
+        #sleep for a second so website doesn't think I'm spamming
+        time.sleep(1)
+    return IATA_list
+
+def getIATAURLs():
+    IATA_list = getIATAcodes()
+    url_base = 'https://airports-list.com'
+    url_list = []
+    for code in IATA_list:
+        url = url_base + code
+        url_list.append(url)
+    return url_list
